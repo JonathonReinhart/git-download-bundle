084f44e716cec9f41912642ed586af2988584ef8
Jonathon Reinhart <jreinhart@cc-sw.com>
2015-02-04 13:29:31 -0500

    Add first working version of script

---
+++ download_bundle.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Jonathon Reinhart 2015
+#
+# References:
+# http://stackoverflow.com/questions/1251713
+# http://stackoverflow.com/questions/965053
+set -e
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $(basename $0) git-repo"
+    exit 1
+fi
+
+url=$1                          # e.g. "git@github.com:person/project.git"
+gitdir=$(basename $url)         # e.g. "project.git"
+bundle="${gitdir%.*}.bundle"    # e.g. "project.bundle" 
+
+git clone --mirror $url
+
+git --git-dir $gitdir bundle create $bundle --all
+
+rm -rf $gitdir
